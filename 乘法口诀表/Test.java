d28ba3909db02c4abc6909c1209b22ef9e4fb863
18292677162 <2254095453@qq.com>
2018-10-07 17:34:45 +0800

    -

---
+++ 乘法口诀表/Test.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class Test {
+	public static void main(String[] args) {
+	    PrintTable();
+	}
+	
+	
+	public static void PrintTable() {
+		for(int i = 1;i <= 9;i++) {
+			for(int j = 1;j <= i;j++) {
+				System.out.printf("%d*%d=%2d ", i, j, i * j);
+			}
+			System.out.println();
+		}
+	}
+}
+
